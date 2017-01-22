42ec300ea8dc2bd33b18089e13d5e821014e87e2
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-01-22 21:43:40 +0800

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 var gulp = require('gulp');
-var browsersync = require('browser-sync');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
+gulp.task('test', function () {
+    return console.log('this is a test');
+});
+
+gulp.task('sass', function () {
+    return gulp.src('src/sass/*.scss')
+    .pipe(sass())
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/static/css'));
+});
+
+gulp.task('build', ['test', 'sass']);
