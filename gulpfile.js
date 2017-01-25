a54f1a1992826d930198506f9de4ae7bd698f7bc
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-01-26 00:14:41 +0800

    rls

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
  * function: config gulp command
  * create date： 2017-01-25
  * last modified: 2017-01-25
+ * author: Allen Wong
  */
 
 /**
@@ -19,6 +20,7 @@ const cleanCss = require('gulp-clean-css')
 const babel = require('gulp-babel')
 const rimraf = require('gulp-rimraf')
 const ignore = require('gulp-ignore')
+const htmlMinify = require('gulp-html-minify')
 
 /** browser-sync */
 const browserSync = require('browser-sync').create()
@@ -91,6 +93,39 @@ gulp.task('del:dev', () => {
 
 /**============ develop command(end) =================================*/
 
+/**============ release command ======================================*/
+
+/** copy template html files from build to dist */
+gulp.task('tpl:rls', () => {
+    return gulp.src('build/tpl/*.html')
+    .pipe(htmlMinify())
+    .pipe(gulp.dest('dist/tpl'))
+})
+
+/** compile scss files to css */
+gulp.task('sass:dev', () => {
+    return gulp.src('src/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build/static/css'))
+    .pipe(reload({ stream: true }))
+})
+
+/** copy js file from src to build */
+gulp.task('js:dev', () => {
+    return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('build/static/js'))
+    .pipe(reload({ stream: true }))
+})
+
+/** delete whateaver in folder build */
+gulp.task('del:dev', () => {
+    return gulp.src('build/**', { read: false })  //fast way to delete
+    .pipe(ignore('build/README.md'))
+    .pipe(rimraf({ force: true }))
+})
+
+/**============ release command(end) =================================*/
+
 /** final command for development */
 gulp.task('dev', ['tpl:dev', 'sass:dev', 'js:dev'], () => {
     // browser-sync will start in a browser automatically
@@ -108,3 +143,6 @@ gulp.task('dev', ['tpl:dev', 'sass:dev', 'js:dev'], () => {
 
 /** final command for dist */
 gulp.task('build', ['del', 'tpl', 'sass', 'js'])
+
+/** release command for dist */
+gulp.task('release', ['del', 'tpl:rls', 'css:rls', 'js:rls'])
